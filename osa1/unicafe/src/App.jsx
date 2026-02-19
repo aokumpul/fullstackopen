07843b6b2e9a8a0e1f67f6f35fa7c38c8a61e604
aokumpul <260420908+aokumpul@users.noreply.github.com>
2026-02-19 22:28:03 +0200

    1.10

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -6,6 +6,14 @@ const Button = (props) => (
   </button>
 )
 
+const StatisticLine = ({ text, value, unit }) => {
+  return (
+    <div>
+      <p>{text} {value} {unit}</p>
+    </div>
+  )
+} 
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   const average = (good - bad) / total
@@ -20,12 +28,12 @@ const Statistics = ({ good, neutral, bad }) => {
   }
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} unit="%" />
     </div>
   )
 }
