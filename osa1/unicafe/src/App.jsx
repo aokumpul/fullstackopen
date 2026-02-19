3cfac19900512a3e1f107230d60188690b87618e
aokumpul <260420908+aokumpul@users.noreply.github.com>
2026-02-19 22:40:14 +0200

    1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -8,9 +8,14 @@ const Button = (props) => (
 
 const StatisticLine = ({ text, value, unit }) => {
   return (
-    <div>
-      {text} {value} {unit}
-    </div>
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value} {unit}
+      </td>
+    </tr>
   )
 } 
 
@@ -27,14 +32,16 @@ const Statistics = ({ good, neutral, bad }) => {
     )
   }
   return (
-    <div>
+    <table>
+      <tbody>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
       <StatisticLine text="all" value={total} />
       <StatisticLine text="average" value={average} />
       <StatisticLine text="positive" value={positive} unit="%" />
-    </div>
+      </tbody>
+    </table>
   )
 }
 
