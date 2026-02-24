a95d234d0d5e505bc961a6d2cacf970b23da2149
aokumpul <260420908+aokumpul@users.noreply.github.com>
2026-02-24 19:17:48 +0200

    2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,41 +1,4 @@
-const Total = ({parts}) => {
-  const total = parts.reduce( (sum, part) => sum + part.exercises, 0)
-  return (
-    <p>
-      <strong>total of {total} exercises</strong>
-      </p>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Header = ({header}) => {
-  return (
-    <h2>{header}</h2>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header header={course.name}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
