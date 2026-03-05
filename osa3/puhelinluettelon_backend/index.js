7ff62370a3b6e438bf7d163cf0a4f05739df8d11
aokumpul <260420908+aokumpul@users.noreply.github.com>
2026-03-05 21:57:11 +0200

    3.8

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('post-data', (req) => {
+    return req.method === 'POST' ? JSON.stringify(req.body) : '';
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :post-data'))
 
 let persons = [
     {
