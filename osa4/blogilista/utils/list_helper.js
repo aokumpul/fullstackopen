2705ed045add82e6ce026975427295f4bc3af023
aokumpul <260420908+aokumpul@users.noreply.github.com>
2026-03-12 22:22:43 +0200

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,7 +10,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  
+  const reducer = (favorite, blog) => {
+    if (blog.likes > favorite.likes) {
+      return blog
+    } else {
+      return favorite
+    }
+  }
+
+  return blogs.reduce(reducer, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
