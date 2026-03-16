123096dfbf1d7587d05a0b1830779dfc92ba3cd1
aokumpul <260420908+aokumpul@users.noreply.github.com>
2026-03-16 23:23:01 +0200

    5.1 - 5.4

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import loginService from './services/login'
 import blogService from './services/blogs'
+import Notification from './components/Notification'
 
 const App = () => {
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +21,151 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+
+      setSuccessMessage('successfully logged in')
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+    } catch (error) {
+      const errorMessage = error.response?.data?.error || 'wrong username or password'
+      setErrorMessage(errorMessage)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+      window.localStorage.removeItem('loggedBlogappUser') 
+    
+      setSuccessMessage('you have been logged out, please refresh the page')
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+  }
+
+  const addBlog = async event => {
+    event.preventDefault()
+    try {
+      const newBlog = await blogService.create({ title, author, url })
+      
+      setBlogs(blogs.concat(newBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+
+      setSuccessMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+    } catch (error) {
+      const errorMessage = error.response?.data?.error || 'an error occurred'
+      setErrorMessage(errorMessage)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        <label>
+          title:
+          <input 
+          type="text"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}/>
+        </label>
+      </div>
+      <div>
+        <label>
+          author:
+          <input 
+          type="text"
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}/>
+        </label>
+      </div>
+      <div>
+        <label>
+          url:
+          <input 
+          type="text"
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}/>
+        </label>
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <Notification message={successMessage} type='success' />
+        <Notification message={errorMessage} type='error' />
+        {loginForm()}
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={successMessage} type='success' />
+      <Notification message={errorMessage} type='error' />
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+      <h2>create new</h2>
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
